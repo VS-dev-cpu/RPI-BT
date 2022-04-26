6b4ab8f7ff961a524af5fb87da3ce3d432b90e67
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-04-26 15:12:19 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,12 @@
+import bt
+import time
+
+bt = bt.BT("basic")
+print("waiting...")
+time.sleep(2)
+print("go")
+bt.start()
+
+while (1):
+    addr, a = bt.receive()
+    print("From " + str(addr) + " Received: " + str(a))
