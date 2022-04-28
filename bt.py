7c52dd1d05f988270bdfe3b89ffa065ce39eaa44
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-04-28 15:35:38 +0200

    Update bt.py

--- bt.py
+++ bt.py
@@ -12,34 +12,21 @@ class BT():
         mac_rpi4_zeti = "DC:A6:32:78:BC:C7"    #RPI3
         mac_rpi4_mate = "DC:A6:32:25:D2:CC"    #RPI4
         
-        self.server = mac_rpi4_samu
-        self.rpi0 = mac_rpi0_samu    #ROOSTER
-        self.rpi1 = mac_rpi3_samu    #PIG 1
-        self.rpi2 = mac_rpi4_zeti    #PIG 2
-        self.rpi3 = mac_rpi4_mate    #PIG 3
+        self.rooster = mac_rpi3_samu
+        self.pig1 = mac_rpi4_samu
+        self.pig2 = mac_rpi4_zeti
+        self.pig3 = mac_rpi4_mate
       
         if (hostname == "server"):
-            
-            on_rpi0 = 0
-            on_rpi1 = 0
-            on_rpi2 = 0
-            on_rpi3 = 0
-            
-            while (on_rpi0 == 0 or on_rpi1 == 0 or on_rpi2 == 0 or on_rpi3 == 0):
-                addr, data = self.receive()
-                if (addr == self.rpi0):
-                    on_rpi0 = 1
-                if (addr == self.rpi1):
-                    on_rpi1 = 1
-                if (addr == self.rpi2):
-                    on_rpi2 = 1
-                if (addr == self.rpi3):
-                    on_rpi3 = 1
+            self.sync()
+            self.sync()
+            self.sync()
                 
-        elif (hostname == "basic"):
-            pass
-        else:
+        elif (hostname == "client"):
             self.send(self.server, "asd")
+            
+        else:
+            pass
     
     def receive(self):
         server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
@@ -67,17 +54,9 @@ class BT():
             print("FAILED TO SEND MESSAGE '" + str(message) + "' TO " + str(targetBluetoothMacAddress))
         
     def sync(self):
-        addr, data = self.receive()
+        return self.receive()
 
     def start(self):
-        if (self.rpi0 != ""):
-            self.send(self.rpi0, "start")
-            
-        if (self.rpi1 != ""):
-            self.send(self.rpi1, "start")
-        
-        if (self.rpi2 != ""):
-            self.send(self.rpi2, "start")
-            
-        if (self.rpi3 != ""):
-            self.send(self.rpi3, "start")
+        self.send(self.pig1, "start")
+        self.send(self.pig2, "start")
+        self.send(self.pig3, "start")
