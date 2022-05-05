25f75744a370a24cdab29e9ec8ba127b94449554
SamTheDev <77497287+VS-dev-cpu@users.noreply.github.com>
2022-05-05 14:28:22 +0200

    Update bt.py

--- bt.py
+++ bt.py
@@ -7,38 +7,17 @@ class BT():
         
         os.system("bluetoothctl discoverable on")
         
-        mac_rpi0_samu = "B8:27:EB:48:52:95"    #RPI0
-        mac_rpi3_samu = "B8:27:EB:10:0D:19"    #RPI1
-        mac_rpi4_samu = "DC:A6:32:6B:3A:AB"    #RPI2
+        self.mac_rpi0_samu = "B8:27:EB:48:52:95"    #RPI0
+        self.mac_rpi3_samu = "B8:27:EB:10:0D:19"    #RPI1
+        self.mac_rpi4_samu = "DC:A6:32:6B:3A:AB"    #RPI2
         
-        mac_rpi4_zeti = "DC:A6:32:78:BC:C7"    #RPI3
-        mac_rpi4_mate = "DC:A6:32:25:D2:CC"    #RPI4
+        self.mac_rpi4_zeti = "DC:A6:32:78:BC:C7"    #RPI3
+        self.mac_rpi4_mate = "DC:A6:32:25:D2:CC"    #RPI4
         
-        self.rooster = mac_rpi3_samu
-        self.pig1 = mac_rpi4_samu
-        self.pig2 = mac_rpi4_zeti
-        self.pig3 = mac_rpi4_mate
-        
-        self.available = []
-      
-        if (hostname == "server"):
-            print("waiting...")
-            for i in range(2):
-                addr, _ = self.sync()
-                self.available.append(addr)
-                print("response " + str(i) + " by " + str(addr))
-                        
-            self.start()
-                           
-        elif (hostname == "client"):
-            en = False
-            while not en:
-                en = self.send(self.rooster, "asd")
-                
-            self.sync()
-            
-        else:
-            pass
+        self.rooster = self.mac_rpi3_samu
+        self.pig1 = self.mac_rpi4_samu
+        self.pig2 = self.mac_rpi4_zeti
+        self.pig3 = self.mac_rpi4_mate
     
     def receive(self):
         server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
@@ -72,5 +51,5 @@ class BT():
         return self.receive()
 
     def start(self):
-        for i in self.available:
-            self.send(i, "go")
+        self.send(self.mac_rpi0_samu, "go")
+        self.send(self.mac_rpi3_samu, "go")
